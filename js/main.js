9302fadd6e700741dc0cbf8c3ae0346fd1fd6c0f
Neha <nehatembhare20@gmail.com>
2024-07-15 15:27:21 +0530

    doctor consultation dashboad design

---
+++ js/main.js
@@ -0,0 +1,406 @@
+
+(function ($) {
+  // USE STRICT
+  "use strict";
+
+  try {
+    
+    //WidgetChart 3
+    var ctx = document.getElementById("widgetChart3");
+    if (ctx) {
+      ctx.height = 130;
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: ['May', 'May', 'May', 'May', 'May', 'May'],
+          type: 'line',
+          datasets: [{
+            data: [80, 75, 82, 77, 80, 79],
+            label: 'Dataset',
+            backgroundColor: 'transparent',
+            borderColor: 'rgba(44,47,146,255)',
+          },]
+        },
+        options: {
+
+          maintainAspectRatio: false,
+          legend: {
+            display: false
+          },
+          responsive: true,
+          tooltips: {
+            mode: 'index',
+            titleFontSize: 12,
+            titleFontColor: '#000',
+            bodyFontColor: '#000',
+            backgroundColor: '#fff',
+            titleFontFamily: 'Montserrat',
+            bodyFontFamily: 'Montserrat',
+            cornerRadius: 3,
+            intersect: false,
+          },
+          scales: {
+            xAxes: [{
+              gridLines: {
+                color: 'transparent',
+                zeroLineColor: 'transparent'
+              },
+              ticks: {
+                fontSize: 2,
+                fontColor: 'transparent'
+              }
+            }],
+            yAxes: [{
+              display: false,
+              ticks: {
+                display: false,
+              }
+            }]
+          },
+          title: {
+            display: false,
+          },
+          elements: {
+            line: {
+              borderWidth: 1
+            },
+            point: {
+              radius: 4,
+              hitRadius: 10,
+              hoverRadius: 4
+            }
+          }
+        }
+      });
+    }
+
+
+    // Recent Report
+    const brandProduct = 'rgba(0,181,233,0.8)'
+    const brandService = 'rgba(0,173,95,0.8)'
+
+    var elements = 10
+    var data1 = [52, 60, 55, 50, 65, 80, 57, 70, 105, 115]
+    var data2 = [102, 70, 80, 100, 56, 53, 80, 75, 65, 90]
+
+    var ctx = document.getElementById("recent-rep-chart");
+    if (ctx) {
+      ctx.height = 250;
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', ''],
+          datasets: [
+            {
+              label: 'My First dataset',
+              backgroundColor: brandService,
+              borderColor: 'transparent',
+              pointHoverBackgroundColor: '#fff',
+              borderWidth: 0,
+              data: data1
+
+            },
+            {
+              label: 'My Second dataset',
+              backgroundColor: brandProduct,
+              borderColor: 'transparent',
+              pointHoverBackgroundColor: '#fff',
+              borderWidth: 0,
+              data: data2
+
+            }
+          ]
+        },
+        options: {
+          maintainAspectRatio: true,
+          legend: {
+            display: false
+          },
+          responsive: true,
+          scales: {
+            xAxes: [{
+              gridLines: {
+                drawOnChartArea: true,
+                color: '#f2f2f2'
+              },
+              ticks: {
+                fontFamily: "Poppins",
+                fontSize: 12
+              }
+            }],
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+                maxTicksLimit: 5,
+                stepSize: 50,
+                max: 150,
+                fontFamily: "Poppins",
+                fontSize: 12
+              },
+              gridLines: {
+                display: true,
+                color: '#f2f2f2'
+
+              }
+            }]
+          },
+          elements: {
+            point: {
+              radius: 0,
+              hitRadius: 10,
+              hoverRadius: 4,
+              hoverBorderWidth: 3
+            }
+          }
+
+
+        }
+      });
+    }
+
+    // Percent Chart
+    var ctx = document.getElementById("percent-chart");
+    if (ctx) {
+      ctx.height = 200;
+      var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+          datasets: [
+            {
+              label: "My First dataset",
+              data: [60, 30, 10],
+              backgroundColor: [
+                '#00a652',
+                '#ec7652',
+                '#113d94'
+              ],
+              hoverBackgroundColor: [
+                '#00a652',
+                '#ec7652',
+                '#113d94'
+              ],
+              borderWidth: [
+                0, 0, 0
+              ],
+              hoverBorderColor: [
+                'transparent',
+                'transparent',
+                'transparent'
+              ]
+            }
+          ],
+          labels: [
+            'A',
+            'C',
+            'D',
+
+          ]
+        },
+        options: {
+          maintainAspectRatio: false,
+          responsive: true,
+          cutoutPercentage: 55,
+          animation: {
+            animateScale: true,
+            animateRotate: true
+          },
+          legend: {
+            display: false
+          },
+          tooltips: {
+            titleFontFamily: "Poppins",
+            xPadding: 15,
+            yPadding: 10,
+            caretPadding: 0,
+            bodyFontSize: 16
+          }
+        }
+      });
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+
+
+})(jQuery);
+
+
+
+(function ($) {
+    // USE STRICT
+    "use strict";
+    var navbars = ['header', 'aside'];
+    var hrefSelector = 'a:not([target="_blank"]):not([href^="#"]):not([class^="chosen-single"])';
+    var linkElement = navbars.map(element => element + ' ' + hrefSelector).join(', ');
+    $(".animsition").animsition({
+      inClass: 'fade-in',
+      outClass: 'fade-out',
+      inDuration: 900,
+      outDuration: 900,
+      linkElement: linkElement,
+      loading: true,
+      loadingParentElement: 'html',
+      loadingClass: 'page-loader',
+      loadingInner: '<div class="page-loader__spin"></div>',
+      timeout: false,
+      timeoutCountdown: 5000,
+      onLoadEvent: true,
+      browser: ['animation-duration', '-webkit-animation-duration'],
+      overlay: false,
+      overlayClass: 'animsition-overlay-slide',
+      overlayParentElement: 'html',
+      transition: function (url) {
+        window.location.href = url;
+      }
+    });
+  
+  
+  })(jQuery);
+(function ($) {
+  // USE STRICT
+  "use strict";
+  
+})(jQuery);
+(function ($) {
+  // Use Strict
+  "use strict";
+  try {
+    var progressbarSimple = $('.js-progressbar-simple');
+    progressbarSimple.each(function () {
+      var that = $(this);
+      var executed = false;
+      $(window).on('load', function () {
+
+        that.waypoint(function () {
+          if (!executed) {
+            executed = true;
+            /*progress bar*/
+            that.progressbar({
+              update: function (current_percentage, $this) {
+                $this.find('.js-value').html(current_percentage + '%');
+              }
+            });
+          }
+        }, {
+            offset: 'bottom-in-view'
+          });
+
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+})(jQuery);
+(function ($) {
+  // USE STRICT
+  "use strict";
+
+  // Scroll Bar
+  try {
+    var jscr1 = $('.js-scrollbar1');
+    if(jscr1[0]) {
+      const ps1 = new PerfectScrollbar('.js-scrollbar1');      
+    }
+
+    var jscr2 = $('.js-scrollbar2');
+    if (jscr2[0]) {
+      const ps2 = new PerfectScrollbar('.js-scrollbar2');
+
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+
+})(jQuery);
+(function ($) {
+  // USE STRICT
+  "use strict";
+
+  // Select 2
+  try {
+
+    $(".js-select2").each(function () {
+      $(this).select2({
+        minimumResultsForSearch: 20,
+        dropdownParent: $(this).next('.dropDownSelect2')
+      });
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+
+
+})(jQuery);
+(function ($) {
+  // USE STRICT
+  "use strict";
+
+  
+
+  var wW = $(window).width();
+    // Right Sidebar
+    var right_sidebar = $('.js-right-sidebar');
+    var sidebar_btn = $('.js-sidebar-btn');
+
+    sidebar_btn.on('click', function (e) {
+      e.preventDefault();
+      for (var i = 0; i < menu.length; i++) {
+        menu[i].classList.remove("show-dropdown");
+      }
+      sub_menu_is_showed = -1;
+      right_sidebar.toggleClass("show-sidebar");
+    });
+
+    $(".js-right-sidebar, .js-sidebar-btn").click(function (event) {
+      event.stopPropagation();
+    });
+
+    $("body,html").on("click", function () {
+      right_sidebar.removeClass("show-sidebar");
+
+    });
+ 
+
+  
+
+
+  try {
+    // Hamburger Menu
+    $('.hamburger').on('click', function () {
+      $(this).toggleClass('is-active');
+      $('.navbar-mobile').slideToggle('500');
+    });
+    $('.navbar-mobile__list li.has-dropdown > a').on('click', function () {
+      var dropdown = $(this).siblings('ul.navbar-mobile__dropdown');
+      $(this).toggleClass('active');
+      $(dropdown).slideToggle('500');
+      return false;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})(jQuery);
+(function ($) {
+  // USE STRICT
+  "use strict";
+
+
+})(jQuery);
+(function ($) {
+  // USE STRICT
+  "use strict";
+
+  try {
+    
+    $('[data-toggle="tooltip"]').tooltip();
+
+  } catch (error) {
+    console.log(error);
+  }
+
+  
+
+})(jQuery);
